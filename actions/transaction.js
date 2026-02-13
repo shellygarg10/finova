99059612a27447cfba66ef8ce0a4bb117ff89e02
shellygarg10 <shellygarg3522@gmail.com>
2026-02-13 14:50:22 +0530

    model update

--- actions/transaction.js
+++ actions/transaction.js
@@ -8,7 +8,7 @@ import aj from "@/lib/arcjet";
 import { request } from "@arcjet/next";
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-flash-latest" });
 
 const serializeAmount = (obj) => ({
   ...obj,
